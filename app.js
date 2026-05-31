91fa4ce4141b2e0847d7ff03a53d066090f9c184
AGMHSS <agmhsspatteeswaram@gmail.com>
2026-05-31 14:19:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 const APP_CONFIG = {
     fullName: "ORIENTAL GAHSS", 
     shortName: "ORIENTAL GAHSS",                                           
-    scriptUrl: "https://script.google.com/macros/s/AKfycbwAYUxAZ7GWI-GgMcsZJ7MjIjhgFTulAVXR4lyqK0QWIhcXh-g1AoxGyp5xQxWhgzIJvg/exec" 
+    scriptUrl: "https://script.google.com/macros/s/AKfycby5AWmV6uxA09viH5ooofU7pI7Tc3gwjTECenaqoI1u9qD-RqNYWQBcNbSuk0hpb0ORJg/exec" 
 };
 const SCRIPT_URL = APP_CONFIG.scriptUrl;
 
